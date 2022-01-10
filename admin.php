c8b748d703697f2c6651db143358dd98661cb81b
jimmahinya1@gmail.com <tremainea36@gmail.com>
2022-01-10 16:32:11 +0300

    changed info

--- admin.php
+++ admin.php
@@ -663,7 +663,6 @@ $retrieve = mysqli_query($db,$sqluse);
               <li class="treeview">
               	  <a data-toggle='modal' href="#Taxreceipted" class="Open-Taxreceipted"><i class='fa fa-print'></i>Bulk printing</a>
                </li>
-                     //table
                <li class="treeview">
                 <a href="#">
                 <i class="fa fa-cog"></i>
