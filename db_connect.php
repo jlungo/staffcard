531dd9f2e2cf61f747893794bbd19ffb1ae75312
jmganga19 <jmganga19@yahoo.com>
2022-01-10 11:37:45 +0300

    new group3 staffcard

--- db_connect.php
+++ db_connect.php
@@ -1,75 +1,97 @@
 <?php
+ 
+ $db = new mysqli("localhost","root","");
+   if($db->connect_errno > 0){
+         die('Unable to connect to database [' . $db->connect_error . ']');  } 
 
-//connected to the database
-$db = new mysqli("localhost", "root", "", "staff_attendance");
-if ($db->connect_errno > 0) {
-  die('Unable to connect to database [' . $db->connect_error . ']');
+//connection to  push promotion info
+$servername ='localhost';
+$username='root';
+$password='';
+$dbname ="staff_db";
+
+//create connection
+$conn = mysqli_connect($servername,$username,$password,"$dbname");
+if(!$conn){
+    die('could not connect my Sql:' .mysql_error());
 }
+     
+	 $db->query("CREATE DATABASE IF NOT EXISTS `staff_db`");
+	 
+             mysqli_select_db($db,"staff_db");
+             
+   
+        		                       
+		    
+		$stableYZ="CREATE TABLE IF NOT EXISTS Inorg (id int(11) NOT NULL auto_increment,
+                 name varchar(300)NOT NULL,Phone varchar(300)NOT NULL,email varchar(100)NOT NULL,
+                 website varchar(300)NOT NULL,year varchar(10)NOT NULL,pname varchar(1000)NOT NULL,type varchar(30)NOT NULL,
+                 size decimal(10)NOT NULL,content longblob NOT NULL, PRIMARY KEY(id) )";
+               $db->query($stableYZ);
+							 
+			   $stableZ="CREATE TABLE IF NOT EXISTS Profilepictures (id int(11) NOT NULL auto_increment,
+                 ids varchar(30)NOT NULL,Category varchar(30)NOT NULL,name varchar(1000)NOT NULL,type varchar(30)NOT NULL,
+                 Size decimal(10)NOT NULL,content longblob NOT NULL,
+                 PRIMARY KEY(id) )";
+               $db->query($stableZ);
+			   
+			    $stable1="CREATE TABLE IF NOT EXISTS Files (id int(11) NOT NULL auto_increment,
+                                  Title varchar(300)NOT NULL,Name varchar(1000)NOT NULL,
+                                 Type varchar(30)NOT NULL,Size decimal(10) NULL,
+                                   content longblob NOT NULL,PRIMARY KEY(id) )";
+                                 $db->query( $stable1);
+               
+                  $stable56="CREATE TABLE IF NOT EXISTS Users (id int(11) NOT NULL auto_increment,
+                                  Firstname varchar(300)NOT NULL, 
+                                  Sirname varchar(300)NOT NULL,
+                                  Mtitle Varchar(30)NOT NULL,                                 
+                                  Rank varchar(30)NOT NULL,                                 
+                                  Department varchar(300)NOT NULL,
+                                  Email varchar(300)NOT NULL,
+                                  Staffid varchar(300)NOT NULL,
+                                  Online varchar(300)NOT NULL,
+                                  Picname varchar(1000)NOT NULL,
+                                  Time bigint(30)NOT NULL,                         
+                                  PRIMARY KEY(id) )";
+                         $db->query($stable56); 
 
-$db->query("CREATE DATABASE IF NOT EXISTS `staff_db`");
+                         $stable03="CREATE TABLE IF NOT EXISTS ProIS (emp_no int(11) NOT NULL auto_increment,
+                
+                                  emp_name varchar(300)NOT NULL,
+                                  job_desc Varchar(30)NOT NULL,                                 
+                                  current_rank varchar(30)NOT NULL,  
+                                  new_rank varchar(300)NOT NULL,                               
+                                  date_promoted date NOT NULL,
+                                  
+                                  high_ed varchar(300)NOT NULL,
+                                  PRIMARY KEY(emp_no) )";
+                         $db->query($stable03); 
+                         
+                        
+			   
+			    $stable4="CREATE TABLE IF NOT EXISTS Administrator (id int(11) NOT NULL auto_increment,
+                                  Firstname varchar(30)NOT NULL,Sirname varchar(30)NOT NULL,Mtitle Varchar(30)NOT NULL,
+                                  Phone varchar(30)NOT NULL,Password varchar(30)NOT NULL,Email varchar(30)NOT NULL,PRIMARY KEY(id) )";
+                                      $db->query($stable4);
+						
+			 	 	
+		
+					$sql="SELECT * FROM Administrator ";					
+                   $result=mysqli_query($db,$sql);
+                   $rowcount=mysqli_num_rows($result);
+                     
+                       if($rowcount==0)
+                         {
+                           $enter="INSERT INTO Administrator (Password,Email,Firstname,Sirname,Mtitle,Phone) VALUES('admin','admin@gmail.com','Patrick','Mvuma','Mr','265999107724')";
+                                  $db->query($enter);
+								  
+						   
+                                                    $querydy = "INSERT INTO Files (Title,Name,Size,Type) ".
+                                 "VALUES ('Staff','staff.csv','76','application/vnd.ms-excel')";                                 
+                                     $db->query($querydy) or die('Errorr, query failed to upload');	
+                                  
+                          }
+                     
+					 		
 
-mysqli_select_db($db, "staff_db");
-
-//below is  a table of activities and supervisors
-$stableYZ = "CREATE TABLE IF NOT EXISTS Activities (id int(11) NOT NULL auto_increment,
-              activityname varchar(100)NOT NULL,supervisor varchar(100)NOT NULL,content longblob NOT NULL, PRIMARY KEY(id) )";
-$db->query($stableYZ);
-
-$stableZ = "CREATE TABLE IF NOT EXISTS Profilepictures (id int(11) NOT NULL auto_increment,
-              ids varchar(30)NOT NULL,Category varchar(30)NOT NULL,name varchar(1000)NOT NULL,type varchar(30)NOT NULL,
-              Size decimal(10)NOT NULL,content longblob NOT NULL,
-              PRIMARY KEY(id) )";
-$db->query($stableZ);
-
-$stable1 = "CREATE TABLE IF NOT EXISTS Files (id int(11) NOT NULL auto_increment,
-              Title varchar(300)NOT NULL,Name varchar(1000)NOT NULL,
-              Type varchar(30)NOT NULL,Size decimal(10) NULL,
-              content longblob NOT NULL,PRIMARY KEY(id) )";
-$db->query($stable1);
-
-$stable56 = "CREATE TABLE IF NOT EXISTS Users (id int(11) NOT NULL auto_increment,
-              Firstname varchar(300)NOT NULL, 
-              Sirname varchar(300)NOT NULL,
-              Mtitle Varchar(30)NOT NULL,                                 
-              Rank varchar(30)NOT NULL,                                 
-              Department varchar(300)NOT NULL,
-              Email varchar(300)NOT NULL,
-              Staffid varchar(300)NOT NULL,
-              Online varchar(300)NOT NULL,
-              Picname varchar(1000)NOT NULL,
-              Time bigint(30)NOT NULL,                         
-              PRIMARY KEY(id) )";
-$db->query($stable56);
-
-//Next of kin information table
-$stable97 = "CREATE TABLE IF NOT EXISTS Next_Of_Kin_Information (id int(11) NOT NULL auto_increment,
-              Emp_No varchar(300)NOT NULL, 
-              Emp_Name varchar(300)NOT NULL,
-              Next_Of_Kin_Name Varchar(300)NOT NULL,                                 
-              Next_Of_kin_Relationship varchar(30)NOT NULL,                                 
-              Next_Of_kin_Sex varchar(300)NOT NULL,
-              Next_Of_Kin_Address varchar(300)NOT NULL,
-              Next_Of_Kin_Mobile_Number varchar(300)NOT NULL,                        
-              PRIMARY KEY(id) )";
-
-$db->query($stable97);
-
-$stable4 = "CREATE TABLE IF NOT EXISTS Administrator (id int(11) NOT NULL auto_increment,
-              Firstname varchar(30)NOT NULL,Sirname varchar(30)NOT NULL,Mtitle Varchar(30)NOT NULL,
-              Phone varchar(30)NOT NULL,Password varchar(30)NOT NULL,Email varchar(30)NOT NULL,PRIMARY KEY(id) )";
-$db->query($stable4);
-
-
-$sql = "SELECT * FROM Administrator ";
-$result = mysqli_query($db, $sql);
-$rowcount = mysqli_num_rows($result);
-
-if ($rowcount == 0) {
-  $enter = "INSERT INTO Administrator (Password,Email,Firstname,Sirname,Mtitle,Phone) VALUES('admin','admin@gmail.com','Patrick','Mvuma','Mr','265999107724')";
-  $db->query($enter);
-
-
-  $querydy = "INSERT INTO Files (Title,Name,Size,Type) " .
-    "VALUES ('Staff','staff.csv','76','application/vnd.ms-excel')";
-  $db->query($querydy) or die('Errorr, query failed to upload');
-}
+?>
--- promotion_report.php
+++ promotion_report.php
@@ -1,6 +1,9 @@
 <?php 
 session_start();
-include("db_connect.php");
+include_once('db_connect.php');
+   
+   $result = mysqli_query($db, "select * from ProIS" );
+
 
 if(isset($_COOKIE['adminid'])&&$_COOKIE['adminemail']){
 	
@@ -28,7 +31,7 @@ $retrieved = mysqli_query($db,$sqluser);
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>admin</title>
+<title>Promotion Report</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Glance Design Dashboard Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
@@ -388,9 +391,7 @@ $retrieve = mysqli_query($db,$sqluse);
              
         	<form method="post" action="upload.php" enctype='multipart/form-data'>        		
             
-        	      <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;
-                Firstname:<label style="color: red;font-size:20px;">*</label>
-                <input style="width:270px;" type="text" name="mfname" id='oldname'></span></p>
+        	      <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;Firstname:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="mfname" id='oldname'></span></p>
         	    <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp; &nbsp;Sirname:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="msname" id='ss'></span></p>
         		<p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">Department:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="minstitution"  id='cc'></span></p>
         	     <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Rank:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="mrank" id='dd'></span></p>
@@ -519,8 +520,8 @@ $retrieve = mysqli_query($db,$sqluse);
        </form>
   </div>
   </div>
-
-  <div id="add_promotion" class="modal fade" role="dialog">
+  <!--created a modal for add pension  -->
+  <div id="add_pension" class="modal fade" role="dialog">
   <div class="modal-dialog">
     <!-- Modal content-->
     <div class="modal-content" style="font-size: 14px; font-family: Times New Roman;color:black;">
@@ -530,43 +531,29 @@ $retrieve = mysqli_query($db,$sqluse);
         	ADD PROMOTION INFORMATION
         	</center></h4>
       </div>
-      	<form method="post" action="process.php" enctype='multipart/form-data'>        		
+      	<form method="post" action="upload.php" enctype='multipart/form-data'>        		
 
       <div class="modal-body" >       	
       	<center> 
-        		<p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp;&nbsp;
-            <label for="emp_no" style="color: red;font-size:20px;">Emp No:*</label><br>
-            <input style="width:270px;" type="text" name="emp_no" ></span></p><br>
-        	    <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;
-              <label for="emp_name" style="color: red;font-size:20px;">Emp Name:*</label><br>
-              <input style="width:270px;" type="text" name="emp_name" ></span></p><br>
-        		<p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
-            <label for="job_desc" style="color: red;font-size:20px;">Job description:*</label><br>
-            <input style="width:270px;" type="text" name="job_desc" ></span></p><br>
-        	     <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp;&nbsp;
-               <label for ="current_rank" style="color: red;font-size:20px;">Current rank:*</label><br>
-               <input style="width:270px;" type="text" name="current_rank" ></span></p><br>
-        	     <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">
-               <label for="new_rank" style="color: red;font-size:20px;">New promotion rank:*</label><br>
-               <input style="width:270px;" type="text" name="new_rank" ></span></p><br>
-        	     <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">
-               <label for="date_promoted" style="color: red;font-size:20px;">Date promoted:*</label><br>
-               <input style="width:270px;" type="date" name="date_promoted" ></span></p><br>
-        	     <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">
-               <label style="color: red;font-size:20px;">Highest education Level:*</label><br>
-               <input style="width:270px;" type="text" name="high_ed" ></span></p><br>
+        		<p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp;&nbsp;Emp No:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="empno"></span></p>
+        	    <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;Emp Name:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="empname"></span></p>
+        		<p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;Pension Type:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="ptype"></span></p>
+        	     <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">&nbsp; &nbsp;&nbsp;&nbsp;Pension Number:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="pnumber"></span></p>
+        	     <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">Registered Date:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="date" name="regdate"></span></p>
+        	     <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">Monthly Contribution:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="mon_contribution"></span></p>
+        	     <p style="margin-bottom:10px;"><span style="font-size: 18px; font-weight: bold;">Current Balance:<label style="color: red;font-size:20px;">*</label><input style="width:270px;" type="text" name="curr_balance"></span></p>
                                    	 <input type="hidden" name="page" value="admin.php"/>                                                        	      		
          </center>
       </div>
       <div class="modal-footer">
-        <input type="submit" class="btn btn-success" value="submit" id="addmember" name="save"> &nbsp;
+        <input type="submit" class="btn btn-success" value="Submit" id="addmember" name="orginitial"> &nbsp;
         <button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
       </div>
       </div>
        </form>
   </div>
   </div>
-  
+ 
  <div id="Initialisation2" class="modal fade" role="dialog">
   <div class="modal-dialog">
     <!-- Modal content-->
@@ -679,7 +666,7 @@ $retrieve = mysqli_query($db,$sqluse);
               <li class="treeview">
               	  <a data-toggle='modal' href="#Taxreceipted" class="Open-Taxreceipted"><i class='fa fa-print'></i>Bulk printing</a>
                </li>
-                     
+                     <!--menu for pension informartion -->
                <li class="treeview">
                 <a href="#">
                 <i class="fa fa-cog"></i>
@@ -778,7 +765,7 @@ $retrieve = mysqli_query($db,$sqluse);
 			<div class="charts">		
 			<div class="mid-content-top charts-grids">
 				<div class="middle-content">
-						<h4 class="title">promotion information</h4>
+						<h4 class="title">Users</h4>
 					<!-- start content_slider -->
 				<div class="alert alert-info">
                              <i class="fa fa-envelope"></i>&nbsp;This screen displays 50 records use the search box to spool more records
@@ -787,51 +774,36 @@ $retrieve = mysqli_query($db,$sqluse);
 					     <table id="example" class="display nowrap" style="width:100%">
         <thead>
             <tr>
-            	
-                <th>Emp No</th>
-                <th>Emp Name</th>
-                <th>Job Desc</th>              
-                <th>Current Rank</th>
-                <th>New Promotion Rank</th>           
-                <th>Date Promoted</th>
-                <th>Highest Education Level</th>
-                
+            
+             <th> Emp No </th>
+             <th> Emp Name </th>
+             <th> Job desc </th>
+             <th> Current Rank </th>
+             <th> New promotion rank </th>
+             <th> Date promoted </th>
+             <th> Highest education level </th> 
             </tr>
         </thead>
         <tbody>
-        	 <?php   $sqlmember ="SELECT * FROM Users ";
-			       $retrieve = mysqli_query($db,$sqlmember);
-				                    $count=0;
-                     while($found = mysqli_fetch_array($retrieve))
-	                 {
-                       $title=$found['Mtitle'];$firstname=$found['Firstname'];$sirname=$found['Sirname'];$rank=$found['Rank'];
-                       $id=$found['id'];$dept=$found['Department'];$contact=$found['Email'];
-			                $count=$count+1;  $get_time=$found['Time']; $time=time(); $pass=$found['Staffid'];
-			              $names=$firstname." ".$sirname;
-					    	 
-			      echo"<tr>    <td>$id</td>                                       
-                             <td>$title $firstname $sirname</td>        	
-                             <td>$pass</td>
-                             <td>$contact</td>
-                             
-			                 <td>$dept</td>
-			                 <td>$rank</td>
-			                 <td>
-			                   <a  href='card.php?id=$id' class='btn  btn-success' title='click to print report' ><span class='glyphicon glyphicon-print' style='color:white;'></span></a>
-                              </td>
-			                 <td>
-			                   <a data-toggle='modal' data-id='$id' data-ie='$firstname'   data-if='$sirname' data-ig='$rank' data-ih='$dept' data-ij='$contact' data-ik='$pass' class='open-Passwords btn  btn-info' title='edit user details' href='#Passwords'><span class='glyphicon glyphicon-edit' style='color:white;'></span></a>
-							 
-			                 </td>				                 
-			                 <td>
-			                   <a data-id='$id'  class='open-Delete btn  btn-danger' title='delete user' ><span class='glyphicon glyphicon-trash' style='color:white;'></span></a>
-							 
-			                 </td>			 
-                             </tr>"; 
-					 
-					 } 
-		
-		           	?>
+        <?php
+      $i=0;
+      while ($rows = mysqli_fetch_assoc($result))
+
+      {
+?>
+  <tr>
+    <th><?php echo $rows["emp_no"]; ?></th>
+    <th><?php echo $rows["emp_name"]; ?></th>
+    <th><?php echo $rows["job_desc"]; ?></th>
+    <th><?php echo $rows["current_rank"]; ?></th>
+    <th><?php echo $rows["new_rank"]; ?></th>
+    <th><?php echo $rows["date_promoted"]; ?></th>
+    <th><?php echo $rows["high_ed"]; ?></th>
+  </tr>
+<?php
+    $i++;  
+    }
+?>
             </tbody>
         
     </table>
