2fdc568d3810a243ae054a7179a7b4ed323795a1
JESSE XIBONA <jxibona@gmail.com>
2022-01-11 12:01:38 +0300

    edited table on database

--- process.php
+++ process.php
@@ -5,13 +5,11 @@ if(isset($_POST['save']))
 {
     $emp_no =$_POST['emp_no'];
     $emp_name =$_POST['emp_name'];
-    $job_desc =$_POST['job_desc'];
-    $current_rank =$_POST['current_rank'];
-    $new_rank =$_POST['new_rank'];
-    $date_promoted =$_POST['date_promoted'];
-    $high_ed =$_POST['high_ed'];
-    $sql = "INSERT INTO prois (emp_no,emp_name,job_desc,current_rank,new_rank,date_promoted,high_ed) 
-    VALUES('$emp_no','$emp_name',' $job_desc','$current_rank', '$new_rank',' $date_promoted',' $high_ed')";
+    $bank_name =$_POST['bank_name'];
+    $bank_branchname =$_POST['bank_branchname'];
+    $bank_accountnumber =$_POST['bank_accountnumber'];
+    $sql = "INSERT INTO bankinfo (emp_no,emp_name,bank_name,bank_branchname,bank_accountnumber) 
+    VALUES('$emp_no','$emp_name',' $bank_name,'$bank_branchname', '$bank_accountnumber')";
     if(mysqli_query($conn,$sql)){
         echo "New record created successfully!";
     } else{
