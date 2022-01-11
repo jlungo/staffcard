46587f8e2e6cd284183b687273e3313b8e5dd5e9
saleh <amirsaleh935@gmail.com>
2022-01-11 18:44:49 +0300

    Added the employee

--- admin.php
+++ admin.php
@@ -85,7 +85,7 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
    <script src='https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js'></script>
    <script src='https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js'></script>
    <script src='https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js'></script>
-   <script src='https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js'></script>
+NN   <script src='https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js'></script>
    <script src='https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js'></script>
 
       <script>
