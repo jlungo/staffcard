5a2572e49faa2e9d838a8596323f3e22e999b331
joshuaminja <joshuaminja5@gmail.com>
2022-01-15 03:29:18 +0300

    Updated the footer copyrights message display

--- promotion_report.php
+++ promotion_report.php
@@ -819,7 +819,7 @@ $retrieve = mysqli_query($db,$sqluse);
 		</div>
 	<!--footer-->
 	<div class="footer">
-	  <p>© 2018 Attainment . All Rights Reserved | Design and developed by mvumapatrick@gmail.com
+	  <p>© 2022 Copyrights . All Rights Reserved
 	
 			</p>		
 	</div>
