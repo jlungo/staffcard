49f5a15723751660a75fd007daee9cfce43ae1eb
Cosaki24 <64184560+Cosaki24@users.noreply.github.com>
2022-01-21 12:59:07 +0300

    new file:   promotion_process.php

---
+++ promotion_process.php
@@ -0,0 +1,27 @@
+<?php 
+
+include_once 'db_connect.php';
+if(isset($_POST['save']))
+{
+
+    $emp_no =$_POST['emp_no'];
+    $emp_name =$_POST['emp_name'];
+    $job_desc =$_POST['job_desc'];
+    $current_rank =$_POST['current_rank'];
+    $new_rank =$_POST['new_rank'];
+    $date_promoted =$_POST['date_promoted'];
+    $high_ed =$_POST['high_ed'];
+    $sql = "INSERT INTO prois (emp_no,emp_name,job_desc,current_rank,new_rank,date_promoted,high_ed) 
+    VALUES('$emp_no','$emp_name',' $job_desc','$current_rank', '$new_rank',' $date_promoted',' $high_ed')";
+    if(mysqli_query($conn,$sql)){
+        header('location:admin.php');
+    } else{
+        echo "Error: " . $sql . " ".mysqli_error($conn);
+    }
+    mysqli_close($conn);
+    
+}
+?>
+
+
+
