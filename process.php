06493a2380faf8403a6b743ff428d50a57bfce13
Godbless-tech <gmosha950@gmail.com>
2022-01-24 15:30:05 +0300

    process file changes

--- process.php
+++ process.php
@@ -4,14 +4,14 @@ if(isset($_POST['save']))
 {   $emp_no =$_POST['emp_no'];
     $emp_name =$_POST['emp_name'];
     $job_desc =$_POST['job_desc'];
-    $current_rank =$_POST['current_rank'];
-    $new_rank =$_POST['new_rank'];
-    $date_promoted =$_POST['date_promoted'];
-    $high_ed =$_POST['high_ed'];
-    $sql = "INSERT INTO prois (emp_no,emp_name,job_desc,current_rank,new_rank,date_promoted,high_ed) 
-    VALUES('$emp_no','$emp_name',' $job_desc','$current_rank', '$new_rank',' $date_promoted',' $high_ed')";
+    $acc_type =$_POST['acc_type'];
+    $acc_desc =$_POST['acc_desc'];
+    $acc_date =$_POST['acc_date'];
+    $any_desc =$_POST['any_desc'];
+    $sql = "INSERT INTO workplace (emp_no,emp_name,job_desc,acc_type,acc_desc,acc_date,any_desc) 
+    VALUES('$emp_no','$emp_name','$job_desc','$acc_type', '$acc_desc', '$acc_date', '$any_desc')";
     if(mysqli_query($conn,$sql)){
-        echo "New promotion record created successfully!";
+        echo "New Accident Incident record created successfully!";
     } else {
         echo "Error: " . $sql . " ".mysqli_error($conn);
     }
