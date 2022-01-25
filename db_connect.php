694a1e1d109b306f02cda018784873994ff9f1c3
Eliza-inn <67258871+Eliza-inn@users.noreply.github.com>
2022-01-25 12:31:26 +0300

    I have change database field Emp_Name to Employee_Name from db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -55,7 +55,7 @@
                                      //Disciplinary Information System Table                           
                                       $stableG8="CREATE TABLE IF NOT EXISTS Disciplinary (id int(11) NOT NULL auto_increment,
                                       Emp_No int(11) NOT NULL,
-                                      Emp_Name varchar(300)NOT NULL, 
+                                      Employee_Name varchar(300)NOT NULL, 
                                       Disciplinary_Type varchar(300)NOT NULL,
                                       Date_commited DATE NOT NULL,
                                       Date_discussed DATE NOT NULL,  
